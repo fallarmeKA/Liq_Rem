49b32c5011afb697c149e9e29653d8e214ccd25d
KYLE FALLARME <kalfallarme@mymail.mapua.edu.ph>
2025-09-19 21:00:26 +0800

    upd signup

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../../../supabase/auth"; // make sure this path is correct
+import { useAuth } from "../../../supabase/auth"; 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
